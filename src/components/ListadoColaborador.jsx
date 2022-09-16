96bac68336d0ae98f142ce9453b24ba889c1a166
enidev911 <enidev911@gmail.com>
2022-09-16 16:45:20 -0300

    up

--- src/components/ListadoColaborador.jsx
+++ src/components/ListadoColaborador.jsx
@@ -3,18 +3,18 @@ import React from "react";
 const ListadoColaborador = ({colaboradores}) => {
   return (
     <div className="col-10 col-md-5 col-lg-8 mx-auto">
-      <div className="card">
+      <div className="card border border-dark">
         <div className="card-header text-secondary">
           <h2 className="card-title">Listado de colaboradores</h2>
         </div>
-        <div className="card-body">
+        <div className="card-body bg-secondary bg-gradient">
           <ul>
             {colaboradores.map((colaborador) => (
               <li
                 key={colaborador.id.toString()}
-                className="bg-light bg-gradient mb-2 rounded p-1 fs-5 border border-secondary"
+                className="bg-light bg-gradient mb-2 rounded p-1 fs-5 border border-secondary text-start"
               >
-                🙍‍♂️ {colaborador.nombre.toLowerCase()} <br /> 📧
+                🙍‍♂️ {colaborador.nombre.toLowerCase()} <br /> 📧{" "}
                 {colaborador.correo}
               </li>
             ))}
