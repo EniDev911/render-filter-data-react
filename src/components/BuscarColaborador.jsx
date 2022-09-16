d70d42d1a3094bde4a3d34c35a1954dd3cc98e0f
enidev911 <enidev911@gmail.com>
2022-09-16 04:35:47 -0300

    up-logo

--- src/components/BuscarColaborador.jsx
+++ src/components/BuscarColaborador.jsx
@@ -8,13 +8,23 @@ const BuscarColaborador = ({ setBusqueda }) => {
     setBusqueda(e.target.value);
   }
 
+
   return (
     <nav className="navbar navbar-light mb-4 p-3 bg-dark text-light">
-      <span className="navbard-brand">Buscar colaboradores</span>
+      <img src="https://raw.githubusercontent.com/EniDev911/assets/main/png/logo/logo_con_bg.png" alt="logo" className="position-absolute end-50" width="60"/>
+      <span className="navbar-brand text-light">
+        Buscar colaboradores
+      </span>
       <input type="text" 
           placeholder="Buscar colaborador"
           onChange={handleOnChangeBusqueda}
+          list="datalistOptions"
       />
+      <datalist id="datalistOptions">
+        <option value="Colaborador 1" />
+        <option value="Colaborador 2" />
+        <option value="Colaborador 3" />
+      </datalist>
     </nav>
   )
 }
